c32be2363d2a8374c13759e487ff56867e125b3a
Alex Rivera <alexis_rivera_31@hotmail.com>
2017-03-05 11:28:05 -0500

    completed todos complete :)

--- app/ngTodo/features/completeTodo/completeTodo.controller.js
+++ app/ngTodo/features/completeTodo/completeTodo.controller.js
@@ -2,6 +2,21 @@ angular
     .module('ngTodo.completeTodo')
     .controller('CompleteTodoCtrl', CompleteTodoCtrl);
 
-function CompleteTodoCtrl() {
+function CompleteTodoCtrl($http) {
     var vm = this;
+    vm.todos = [];
+
+    $http
+        .get('/api')
+        .then(function (response) {
+
+            for (var i = 0; i < response.data.length; i++) {
+                vm.todos.push(new Todo(
+                    response.data[i].todo,
+                    response.data[i].dueDate,
+                    response.data[i].completed,
+                    response.data[i]._id
+                ));
+            }
+        });
 }
--- app/ngTodo/features/listTodo/listTodo.controller.js
+++ app/ngTodo/features/listTodo/listTodo.controller.js
@@ -2,34 +2,50 @@ angular
     .module('ngTodo.listTodo')
     .controller('ListTodoCtrl', ListTodoCtrl);
 
-function ListTodoCtrl($http) {
+function ListTodoCtrl($http, $scope) {
     var vm = this;
 
     //get current Date & remove time to compare to Due dates
     var currentDate = new Date();
     currentDate.setHours(0, 0, 0, 0);
-
     vm.currentDate = currentDate;
+
+    //hold all todos
     vm.todos = [];
+
+    //update completed todos
+    vm.updateCompleted = updateCompleted;
 
     $http
         .get('/api')
-        .then(function(response) {
+        .then(function (response) {
 
             for (var i = 0; i < response.data.length; i++) {
                 vm.todos.push(new Todo(
                     response.data[i].todo,
                     response.data[i].dueDate,
+                    response.data[i].completed,
                     response.data[i]._id
                 ));
             }
 
         });
-    
+
+    function updateCompleted(todo) {
+        //console.log("i was clicked");
+        $http.put('/api', todo)
+            .then(function (res) {
+                console.log(res);
+            }, function (err) {
+                console.log(err);
+            });
+    }
 }
 
-function Todo(todo, dueDate, id) {
+function Todo(todo, dueDate, completed, id) {
     this.todo = todo;
     this.dueDate = dueDate;
+    this.completed = completed;
     this._id = id;
 }
+
